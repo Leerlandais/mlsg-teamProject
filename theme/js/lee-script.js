19b8836923feab01f4774ce4b1bb9022ee779fa0
leerlandais <leerlandais@gmail.com>
2024-06-03 20:11:29 +0200

    name hover : OK

--- theme/js/lee-script.js
+++ theme/js/lee-script.js
@@ -1,9 +1,37 @@
 $(document).ready(function() {
 
-    $(".ourCopyrightNames").one('mouseenter', function() {  
-        // gonna do something fun here ;)
-        console.log($(this).text());
 
-    })
+    $(document).ready(function() {
+        function tagOurNames() {
+            // nice - this is the jQ for removeEventListener
+            $('.ourCopyrightNames').off('mouseenter');
+            
+        
+            $('.ourCopyrightNames').one('mouseenter', function() {
+                console.log($(this).text() + ' activated');
+                
+                // allows me to prevent them being clicked twice in a row
+                $('.ourCopyrightNames').removeClass("active");
+                $(this).addClass('active');
+    
+                // another find! Take all except this (should also work for named elements, I think)
+                $(".ourCopyrightNames").not(this).one('mouseenter', function() {
+                    console.log($(this).text() + ' activated again');
+                    
+                    $('.ourCopyrightNames').removeClass('active');
+                    $(this).addClass('active');
+                    
+    
+                    tagOurNames();
+                });
+            });
+        }
+    
+        
+        tagOurNames();
+    });
 
-});
+
+}); // end ready
+
+
