184146c0a25dc5ea5868b8da958bf4ff7c3d940b
leerlandais <leerlandais@gmail.com>
2024-06-04 06:55:37 +0200

    added bulletin signup

--- theme/js/lee-script.js
+++ theme/js/lee-script.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-
-
-    $(document).ready(function() {
+/*
         function tagOurNames() {
             // nice - this is the jQ for removeEventListener
             $('.ourCopyrightNames').off('mouseenter');
@@ -9,6 +7,11 @@ $(document).ready(function() {
         
             $('.ourCopyrightNames').one('mouseenter', function() {
                 console.log($(this).text() + ' activated');
+            
+                $(this).replaceWith('<span class="current-year"><a href="https://leerlandais.com">Hi there</a></span>')
+                 
+
+               // $(this).text("works")
                 
                 // allows me to prevent them being clicked twice in a row
                 $('.ourCopyrightNames').removeClass("active");
@@ -18,20 +21,39 @@ $(document).ready(function() {
                 $(".ourCopyrightNames").not(this).one('mouseenter', function() {
                     console.log($(this).text() + ' activated again');
                     
+
                     $('.ourCopyrightNames').removeClass('active');
                     $(this).addClass('active');
                     
-    
-                    tagOurNames();
+                    
                 });
             });
         }
     
         
         tagOurNames();
-    });
+  */
 
+
+  /*      
+        function tagOurNames() {
+   
+    $('.current-year').on('mouseenter', function() {
+        console.log($(this).text() + ' activated');
+        $textHolder = $(this).html();
+         console.log($textHolder);
+        $(this).html('<span class="ourCopyrightLinks current-year"><a href="https://leerlandais.com">Hi there</a></span>')
+                
+    });
+    
+}
+
+$(".ourCopyrightLinks").on("mouseleave", function () {
+    console.log("hi there again");
+    $(this).html($textHolder);
+});
+
+tagOurNames();
+*/
 
 }); // end ready
-
-
