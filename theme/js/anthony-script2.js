22d7c89bddb1267d04b94672c0b40ee12bd92878
leerlandais <leerlandais@gmail.com>
2024-06-07 17:45:10 +0200

    test de code sur backup page

--- theme/js/anthony-script2.js
+++ theme/js/anthony-script2.js
@@ -6,9 +6,9 @@ $(document).ready(function () {
     // scroll slides on mouse scroll 
     $('#myCarousel').bind('mousewheel DOMMouseScroll', function (e) {
 
-        if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+        if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {     
             $(this).carousel('next');
-            getNextHeader();
+            getNextHeader();    // un ajout impertinent au code car je ne connais pas les méthodes de AnimateCSS comme cela dessous
         }
 
     });
@@ -17,8 +17,10 @@ $(document).ready(function () {
 //animated  carousel start
 
     //to add  start animation on load for first slide 
-    $(function () {  // petit note : $(function ().... est la même chose que $(document).ready(function() {})
-        $.fn.extend({
+
+    $(function () {  // petit note : $(function (){}) est la même chose que $(document).ready(function() {}) - jQuery aime bien reduire de plus en plus le code
+
+        $.fn.extend({       // ceci est particulaire de AnimateCSS, donc je ne touche pas ()
             animateCss: function (animationName) {
                 var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd onanimationend animationend';
                 this.addClass('animated ' + animationName).one(animationEnd, function () {
@@ -44,21 +46,27 @@ $(document).ready(function () {
 
 // add animation type  from animate.css on the element which you want to animate
 
-
+// attention au nomenclature des elements.... parfois des classe, parfois des ID, 
+/*
 $('.item2 h2').animateCss('swing');
 $('#item3').animateCss('swing');
-
+*/
+// mieux d'utiliser un class général pour ça 
+$('.titSlide').animateCss('swing');     // je n'ai vraiment AUCUNE idée pourquoi ceci ne fonctionnes pas si outre que 'swing'!
 });
     });
+    
 
-function getNextHeader() {
-    console.log("hi");
-    let myFuckingThing = $("#thisFuckingThing");
-    console.log(myFuckingThing);
-    setTimeout(() => {
-        $('.item2 h2').animateCss('swing');
-        $('#item3').animateCss('swing');
-  
+function getNextHeader() {          
+//    console.log("hi"); // petit test pour assurer que ma fonction est appelé
+    $myFuckingThing = $("#thisFuckingThing");        // oui, moi aussi, ça me rendre dingue :)
+    console.log("here it fucking is : ", $myFuckingThing);            // être sur que le putain de chose est là où il le faut
+    setTimeout(() => {                      // un peu de JS car j'en ai plus envie de me casser la tête
+
+                    // tout autre animation, effet désirée ici
+                    // .queue() permis (SI il fonctionne avec le code de Animate.... c'est bien possible qu'ils ont leur propre méthodes pour faire des queue (comme leur animate-delay-3s)
+        $('.titSlide').animateCss('swing');             
+        
     }, 650);  // Pour raison du timing, < 650ms ne marche pas
 }
 
