820d03e9f330e540d3e83b781a630fdee66fc949
leerlandais <leerlandais@gmail.com>
2024-06-08 19:28:15 +0100

    ani fix Index2

--- theme/js/anthony-script2.js
+++ theme/js/anthony-script2.js
@@ -6,23 +6,20 @@ $(document).ready(function () {
     // scroll slides on mouse scroll 
     $('#myCarousel').bind('mousewheel DOMMouseScroll', function (e) {
 
-        if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {     
+        if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
             $(this).carousel('next');
-            getNextHeader();    // un ajout impertinent au code car je ne connais pas les méthodes de AnimateCSS comme cela dessous
         }
 
     });
 
-});
+
 //animated  carousel start
 
     //to add  start animation on load for first slide 
-
-    $(function () {  // petit note : $(function (){}) est la même chose que $(document).ready(function() {}) - jQuery aime bien reduire de plus en plus le code
-
-        $.fn.extend({       // ceci est particulaire de AnimateCSS, donc je ne touche pas ()
+    $(function () {
+        $.fn.extend({
             animateCss: function (animationName) {
-                var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd onanimationend animationend';
+                var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
                 this.addClass('animated ' + animationName).one(animationEnd, function () {
                     $(this).removeClass(animationName);
                 });
@@ -30,54 +27,74 @@ $(document).ready(function () {
         });
         $('#logoMlsg').animateCss('flipInX');
         $('.item1.active h2').animateCss('zoomIn');
-        $('.item1.active p').animateCss('fadeIn');
+        $('.item.active h2').animateCss('zoomIn');
+        $(".bouton1").animateCss("slideInUp");
 
-    
+
+        function animateWithDelay(element, animation, delay) {
+            setTimeout(function () {
+                $(element).animateCss(animation);
+            }, delay);
+
+        }
+        animateWithDelay("#photoPresentation", "fadeInRight", 0);
+        animateWithDelay("#aboutAnimate", "fadeInLeft", 0);
+
+
+    });
+
     //to start animation on  mousescroll , click and swipe 
     $("#myCarousel").on('slide.bs.carousel', function () {
         $.fn.extend({
             animateCss: function (animationName) {
-                var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd onanimationend animationend';
-            this.addClass('animated ' + animationName).one(animationEnd, function () {
-                $(this).removeClass(animationName);
-            });
-        }
+                var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+                this.addClass('animated ' + animationName).one(animationEnd, function () {
+                    $(this).removeClass(animationName);
+                });
+            }
+        });
+
+        // add animation type  from animate.css on the element which you want to animate
+
+        $('.item2 h2').animateCss('swing');
+        $('#item3').animateCss('swing');
+        $('titreSlide3').animateCss('swing');
     });
 
-// add animation type  from animate.css on the element which you want to animate
 
-// attention au nomenclature des elements.... parfois des classe, parfois des ID, 
-/*
-$('.item2 h2').animateCss('swing');
-$('#item3').animateCss('swing');
-*/
-// mieux d'utiliser un class général pour ça 
-$('.titSlide').animateCss('swing');     // je n'ai vraiment AUCUNE idée pourquoi ceci ne fonctionnes pas si outre que 'swing'!
+
+
+
+
+    const target = document.querySelector('#thisSpanIsNotHere');
+
+    let observer = new IntersectionObserver(function(entries) {
+        entries.forEach(function(entry) {
+            if (!entry.isIntersecting) {
+                console.log("<span> Index n'est pas visible");            
+            } else {
+                console.log('<span> Index est  visible'); 
+                function animateWithDelay(element, animation, delay) {
+                    setTimeout(function () {
+                        $(element).animateCss(animation);
+                    }, delay);
+                }
+        animateWithDelay("#card1", "rollIn", 1000);
+        animateWithDelay("#card2", "rollIn", 1200);
+        animateWithDelay("#card3", "rollIn", 1400);
+
+        animateWithDelay("#actu1", "fadeInDown", 2000);
+        animateWithDelay("#actu2", "fadeInUp", 2000);
+        animateWithDelay("#actu3", "fadeInDown", 2000);
+              $("#thisSpanIsNotHere").remove();
+  
+            }
+        });
+    });
+  
+    observer.observe(target);
+  
+  
+
+
 });
-    });
-    
-
-function getNextHeader() {          
-//    console.log("hi"); // petit test pour assurer que ma fonction est appelé
-    $myFuckingThing = $("#thisFuckingThing");        // oui, moi aussi, ça me rendre dingue :)
-    console.log("here it fucking is : ", $myFuckingThing);            // être sur que le putain de chose est là où il le faut
-    setTimeout(() => {                      // un peu de JS car j'en ai plus envie de me casser la tête
-
-                    // tout autre animation, effet désirée ici
-                    // .queue() permis (SI il fonctionne avec le code de Animate.... c'est bien possible qu'ils ont leur propre méthodes pour faire des queue (comme leur animate-delay-3s)
-        $('.titSlide').animateCss('swing');             
-        
-    }, 650);  // Pour raison du timing, < 650ms ne marche pas
-}
-
-/*
-$('.item1 h2').animateCss('zoomIn');
-$('.item1 p').animateCss('fadeIn');
-
-$('.item2 img').animateCss('zoomIn');
-$('.item2 p').animateCss('fadeIn');
-
-$('.item3 img').animateCss('fadeInLeft');
-$('#paraItem3').animateCss('swing');  // paraItem3 n'existe pas ici
-
-*/
